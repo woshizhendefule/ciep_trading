927f69477a4a8cdbf44ea4c5bc3df3985a7946be
woshizhendefule <995019276@qq.com>
2022-11-18 23:42:38 +0800

    first

---
+++ src/main/java/com/chengyu/ciep_trading/CiepTradingApplication.java
@@ -0,0 +1,13 @@
+package com.chengyu.ciep_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiepTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiepTradingApplication.class, args);
+    }
+
+}
