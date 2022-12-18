2121512e34f3a2d1c1c150de222030646010b4d6
woshizhendefule <995019276@qq.com>
2022-12-18 16:49:03 +0800

    docs: update domain.Message

--- src/main/java/com/chengyu/ciep_trading/domain/Message.java
+++ src/main/java/com/chengyu/ciep_trading/domain/Message.java
@@ -4,15 +4,18 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
  * 留言表
+ *
+ * @author CL
  * @TableName message
  */
-@TableName(value ="message")
+@TableName(value = "message")
 @Data
 public class Message implements Serializable {
     /**
